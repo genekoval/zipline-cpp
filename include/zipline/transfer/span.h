62a3244db17ca86981d547ae0522ef5168f58bcb
Gene Koval <genyaremchuk@protonmail.com>
2021-07-25 19:09:34 -0400

    Remove const qualifier from span element type

--- include/zipline/transfer/span.h
+++ include/zipline/transfer/span.h
@@ -6,15 +6,15 @@
 
 namespace zipline {
     template <typename Socket, typename T>
-    struct transfer<Socket, std::span<const T>> {
-        static auto read(Socket& socket) -> std::span<const T> {
+    struct transfer<Socket, std::span<T>> {
+        static auto read(Socket& socket) -> std::span<T> {
             ERROR() << "span does not support reading";
             throw unsupported_transfer_type();
         }
 
         static auto write(
             Socket& socket,
-            const std::span<const T>& span
+            const std::span<T>& span
         ) -> void {
             TRACE() << "write span";
             write_sequence(socket, span);
