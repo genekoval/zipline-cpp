331f81d8c609059e9b03b521a3db75c8fb78f91d
Gene Koval <genyaremchuk@protonmail.com>
2022-07-04 02:17:39 -0400

    Use time point's duration type

--- include/zipline/transfer/time_point.h
+++ include/zipline/transfer/time_point.h
@@ -7,21 +7,17 @@
 namespace zipline {
     template <typename Socket, typename Clock, typename Duration>
     class transfer<Socket, std::chrono::time_point<Clock, Duration>> {
-        using duration = std::chrono::milliseconds;
         using time_point = std::chrono::time_point<Clock, Duration>;
         using underlying_type = int64_t;
     public:
         static auto read(Socket& socket) -> time_point {
-            const auto millis = transfer<Socket, underlying_type>::read(socket);
-            return time_point(duration(millis));
+            const auto value = transfer<Socket, underlying_type>::read(socket);
+            return time_point(Duration(value));
         }
 
         static auto write(Socket& socket, time_point tp) -> void {
-            const auto millis = std::chrono::time_point_cast<duration>(tp)
-                .time_since_epoch()
-                .count();
-
-            transfer<Socket, underlying_type>::write(socket, millis);
+            const auto value = tp.time_since_epoch().count();
+            transfer<Socket, underlying_type>::write(socket, value);
         }
     };
 }
--- src/zipline.test/transfer/time_point.test.cpp
+++ src/zipline.test/transfer/time_point.test.cpp
@@ -14,11 +14,10 @@ class TimePointTest : public SocketTestBase {};
 
 TEST_F(TimePointTest, ReadWrite) {
     const auto original = zipline::test::clock::now();
-    const auto original_millis =
-        std::chrono::time_point_cast<std::chrono::milliseconds>(original);
 
     zipline::test::transfer::write(sock, original);
+
     const auto copy = zipline::test::transfer::read(sock);
 
-    ASSERT_EQ(original_millis, copy);
+    ASSERT_EQ(original, copy);
 }
