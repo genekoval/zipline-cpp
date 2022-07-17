2f290f3f41b68f66cb8ed5f7962abb304af8b800
Gene Koval <genyaremchuk@protonmail.com>
2022-07-17 01:04:35 -0400

    Log time_point read/write

--- include/zipline/transfer/time_point.h
+++ include/zipline/transfer/time_point.h
@@ -12,11 +12,13 @@ namespace zipline {
     public:
         static auto read(Socket& socket) -> time_point {
             const auto value = transfer<Socket, underlying_type>::read(socket);
+            TIMBER_TRACE("read time point: {}", value);
             return time_point(Duration(value));
         }
 
         static auto write(Socket& socket, time_point tp) -> void {
             const auto value = tp.time_since_epoch().count();
+            TIMBER_TRACE("write time point: {}", value);
             transfer<Socket, underlying_type>::write(socket, value);
         }
     };
