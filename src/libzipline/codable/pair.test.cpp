d76a0cdafb83762c97cbd74ecfe09ce677d7a0bb
Gene Koval <genyaremchuk@protonmail.com>
2022-12-06 20:08:38 -0500

    Add coder tests for pair and string types

---
+++ src/libzipline/codable/pair.test.cpp
@@ -0,0 +1,14 @@
+#include "coder.test.hpp"
+
+using PairTest = CoderTest;
+
+TEST_F(PairTest, SameTypes) {
+    using type = std::int16_t;
+    using pair = std::pair<type, type>;
+
+    test(pair { 0, 300 });
+}
+
+TEST_F(PairTest, DifferentTypes) {
+    test(std::pair<std::int64_t, std::string> { 500, "Hello, World!" });
+}
---
+++ src/libzipline/codable/string.test.cpp
@@ -0,0 +1,36 @@
+#include "coder.test.hpp"
+
+using namespace std::literals;
+
+using StringTest = CoderTest;
+
+TEST_F(StringTest, EmptyString) {
+    test(std::string());
+}
+
+TEST_F(StringTest, String) {
+    test(std::string("Hello, World!"));
+}
+
+TEST_F(StringTest, StringView) {
+    [&]() -> ext::detached_task {
+        constexpr auto string = "Hello"sv;
+
+        co_await zipline::encode(string, buffer);
+        const auto value = co_await zipline::decode<std::string>(buffer);
+        const auto view = std::string_view(value);
+
+        EXPECT_EQ(string, view);
+    }();
+}
+
+TEST_F(StringTest, CString) {
+    [&]() -> ext::detached_task {
+        constexpr auto string = "FooBar";
+
+        co_await zipline::encode(string, buffer);
+        const auto value = co_await zipline::decode<std::string>(buffer);
+
+        EXPECT_EQ(std::string_view(string), std::string_view(value));
+    }();
+}
